31c16b7488db0b90b8b1a879e1fa58a67fc0b1c6
accaria <anugerahbarraqi1997@gmail.com>
2025-06-01 02:04:32 +0700

    feat:changes to main display style

--- src/view/hero-section/page.tsx
+++ src/view/hero-section/page.tsx
@@ -1,6 +1,30 @@
 export default function HeroPageView(){
     return (
-        <div className="flex flex-col lg:items-baseline sm:items-center lg:justify-self sm:justify-center p-8 ">
+        <div className="w-full h-full flex flex-col items-center justify-center p-8">
+        <div className="w-7/8 h-7/8 mx-auto border border-amber-100 border-4">
+
+        <div className="flex flex-col font-bold text-4xl items-center justify-center">INTRODUCTION</div>
+
+        <div className="flex p-4 ">
+        <div className="flex flex-col gap-20 ">
+        <div className=" border-amber-100 border-4 w-30 text-center">INTRO</div>
+        <div className=" border-amber-100 border-4 w-30 text-center">ABOUT</div>
+        <div className=" border-amber-100 border-4 w-30 text-center">CONTACT</div>
+        </div>
+        <div className="w-1/1 p-4 ml-4 border-amber-100 border-4">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Neque totam sunt velit ea nesciunt sequi enim. Qui soluta, quaerat illum magni praesentium explicabo tenetur cum sequi, fuga ut cupiditate asperiores?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Neque totam sunt velit ea nesciunt sequi enim. Qui soluta, quaerat illum magni praesentium explicabo tenetur cum sequi, fuga ut cupiditate asperiores?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Neque totam sunt velit ea nesciunt sequi enim. Qui soluta, quaerat illum magni praesentium explicabo tenetur cum sequi, fuga ut cupiditate asperiores?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             </div>
+        </div>
+        </div>
+        </div>
+    );
+} 
+
+
+/*
+<div className="flex flex-col lg:items-baseline sm:items-center lg:justify-self sm:justify-center p-8 ">
         <div className="font-bold text-3xl">
             Hi, my name is <span className="text-amber-500 text-5xl">Muhammad Anugerah</span>
         </div>
@@ -26,5 +50,4 @@ export default function HeroPageView(){
         </div>
         </div>
         </div>
-    );
-} 
+        */
